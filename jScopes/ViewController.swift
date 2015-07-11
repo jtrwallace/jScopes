cf4f75d2cf66cedbf81fd2577f37e5e88e3b4463
Joshua Wallace <jtrwallace@mac.com>
2015-07-10 19:48:22 -0700

    Initial Commit

---
+++ jScopes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jScopes
+//
+//  Created by Joshua Wallace on 7/10/15.
+//  Copyright (c) 2015 jTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ jScopesTests/jScopesTests.swift
@@ -0,0 +1,36 @@
+//
+//  jScopesTests.swift
+//  jScopesTests
+//
+//  Created by Joshua Wallace on 7/10/15.
+//  Copyright (c) 2015 jTech. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class jScopesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
